dc2163ca1778e784d74f452488d7e0dfdc6385a4
Gabriell Huver <gabriell.is.huver@gmail.com>
2020-04-02 12:38:29 -0300

    add test

--- src/test/java/br/com/cep/CepApplicationTests.java
+++ src/test/java/br/com/cep/CepApplicationTests.java
@@ -1,27 +1,33 @@
 package br.com.cep;
 
+import br.com.cep.controller.CepController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-class CepApplicationTests extends AbstractTest {
+@SpringBootTest(classes = CepApplication.class)
+@AutoConfigureMockMvc
+public class CepApplicationTests  {
 
-	@Before
-	void contextLoads() {
-		super.setUp();
-	}
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	private void findCep() throws Exception {
+	public void findCep() throws Exception {
 		MvcResult mvcResult2 = mvc.perform(MockMvcRequestBuilders.get("/cep/01001000")
 				.contentType(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 		int status = mvcResult2.getResponse().getStatus();
@@ -29,7 +35,7 @@ class CepApplicationTests extends AbstractTest {
 	}
 
 	@Test
-	private void findCepsByUfAndIbge() throws Exception {
+	public void findCepsByUfAndIbge() throws Exception {
 		MvcResult mvcResult2 = mvc.perform(MockMvcRequestBuilders.get("/ceps?ibge=3550308&uf=SP")
 				.contentType(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 		int status = mvcResult2.getResponse().getStatus();
@@ -37,3 +43,46 @@ class CepApplicationTests extends AbstractTest {
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
